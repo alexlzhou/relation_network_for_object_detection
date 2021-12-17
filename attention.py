fd82abe879d29f5e880191c63f647c18cbb2d51d
Alex Zhou <alexzhou@ufl.edu>
2021-12-16 23:19:05 -0500

    positional encoding initial

--- attention.py
+++ attention.py
@@ -7,3 +7,30 @@ import torch.nn.functional as F
 
 
 class TransformerModel(nn.Module):
+	def __init__(self, n_token: int, d_model: int, n_head: int, d_hid: int, n_layers: int, dropout: float=0.5):
+		super().__init__()
+		self.model_type = "Transformer"
+
+
+class PositionalEncoding(nn.Module):
+	def __init__(self, d_model: int, dropout: float=0.1, max_len: int=5000):
+		super().__init__()
+		self.dropout = nn.Dropout(p=dropout)
+		
+		position = torch.arange(max_len).unsqueeze(1)
+		div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+		
+		pe = torch.zeros(max_len, 1, d_model)
+		
+		pe[:, 0, 0::2] = torch.sin(position * div_term)
+		pe[:, 0, 1::2] = torch.cos(position * div_term)
+		
+		self.register_buffer('pe', pe)
+	
+	def forward(self, x: Tensor) -> Tensor:
+		"""
+		args:
+			x: Tensor, shape [seq_len, batch_size, embedding_dim]
+		"""
+		x = x + self.pe[:x.size(0)]
+		return self.dropout(x)
